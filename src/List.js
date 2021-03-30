e7ad97dd68cf05b8e0294700a0d475142154ddcd
ShruthiHanuGowda <shruthi.anugowda@gmail.com>
2021-03-30 17:11:28 +0200

    added the delete button for each person

--- src/List.js
+++ src/List.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data";
 
-const List = ({ people }) => {
+const List = () => {
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
   return (
-    <>
-      {people.map((person) => {
-        const { id, name, age, image } = person;
-        return (
-          <article key={id} className="person">
-            <img src={image} alt={name} />
-            <div>
-              <h4>{name}</h4>
-              <p>{age} years</p>
-            </div>
-          </article>
-        );
-      })}
-    </>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+
+        <>
+          {people.map((person) => {
+            const { id, name, age, image } = person;
+            return (
+              <article key={id} className="person">
+                <img src={image} alt={name} />
+                <div>
+                  <h4>{name}</h4>
+                  <p>{age} years</p>
+                  <button onClick={() => removeItem(id)}>Remove</button>
+                </div>
+              </article>
+            );
+          })}
+        </>
+
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
   );
 };
 
