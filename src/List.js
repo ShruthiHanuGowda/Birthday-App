761a8d2dd6fbeef7f517184b0194d5d809d56b46
ShruthiHanuGowda <shruthi.anugowda@gmail.com>
2021-03-31 15:39:45 +0200

    comleted basic functionalitiescommited the changes

--- src/List.js
+++ src/List.js
@@ -9,29 +9,27 @@ const List = () => {
     setPeople(newPeople);
   };
   return (
-    <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
+    <>
+      <h3>{people.length} birthdays today</h3>
 
-        <>
-          {people.map((person) => {
-            const { id, name, age, image } = person;
-            return (
-              <article key={id} className="person">
-                <img src={image} alt={name} />
-                <div>
-                  <h4>{name}</h4>
-                  <p>{age} years</p>
-                  <button onClick={() => removeItem(id)}>Remove</button>
-                </div>
-              </article>
-            );
-          })}
-        </>
+      <>
+        {people.map((person) => {
+          const { id, name, age, image } = person;
+          return (
+            <article key={id} className="person">
+              <img src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+                <button onClick={() => removeItem(id)}>Remove</button>
+              </div>
+            </article>
+          );
+        })}
+      </>
 
-        <button onClick={() => setPeople([])}>Clear All</button>
-      </section>
-    </main>
+      <button onClick={() => setPeople([])}>Clear All</button>
+    </>
   );
 };
 
